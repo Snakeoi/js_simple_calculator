9ce5a5e3a9ebbbb221e77e97e95287bdac218874
Janusz Skotarczak <skotarczak.janusz@gmail.com>
2023-01-21 15:46:18 +0100

    stable calulating

--- js/script.js
+++ js/script.js
@@ -4,6 +4,7 @@ class Calculator {
         this.app = document.getElementById(app)
         this.display_result = this.app.querySelector('.calc__display__result')
         this.display_formula = this.app.querySelector('.calc__display__formula')
+        this.display_error = this.app.querySelector('.calc__error')
         this.operators = []
         this.mapButtons()
     }
@@ -12,19 +13,42 @@ class Calculator {
         const btns_num = this.app.querySelectorAll('.btn-num')
         btns_num.forEach(function(btn){
             btn.addEventListener('click', function () {
-                self.display_formula.innerHTML += btn.innerHTML
+                if (self.display_result.innerHTML !== '0'){
+                    self.display_result.innerHTML = 0
+                    self.display_formula.innerHTML = 0
+                    self.display_formula.innerHTML = btn.innerHTML
+                } else {
+                    self.display_formula.innerHTML += btn.innerHTML
+                }
             });
         }, self=this)
 
         const btns_operator = this.app.querySelectorAll('.btn-operator')
         btns_operator.forEach(function(btn){
             btn.addEventListener('click', function () {
-                const last_char = self.display_formula.innerHTML.slice(-1)
-                if (self.operators.includes(last_char)){
+
+                function is_numeric(str){
+                    return /^\d+$/.test(str);
+                }
+
+                if (self.display_result.innerHTML !== '0'){
+                    self.display_formula.innerHTML = self.display_result.innerHTML
+                    self.display_result.innerHTML = 0
+                } 
+                let last_char = self.display_formula.innerHTML.slice(-1)
+                if (btn.innerHTML === '.'){
+                    if (!is_numeric(last_char)){
+                        self.display_formula.innerHTML += 0
+                        last_char = 0
+                    }
+                }
+                if (self.operators.includes(last_char)){             
                     self.display_formula.innerHTML = self.display_formula.innerHTML.replace(/.$/, btn.innerHTML)
                 } else {
                     self.display_formula.innerHTML += btn.innerHTML
                 }
+
+
             })
             self.operators.push(btn.innerHTML)
         }, self=this)
@@ -34,18 +58,23 @@ class Calculator {
             btn.addEventListener('click', function () {
                 switch (btn.innerHTML) {
                     case 'C':
-                        self.display_formula.innerHTML =''
-                        self.display_result.innerHTML =''
+                        self.display_formula.innerHTML = ''
+                        self.display_result.innerHTML = 0
                         break;
                     case '&lt;':
                         self.display_formula.innerHTML = self.display_formula.innerHTML.replace(/.$/, '')
                         break;
                     case '=':
                         let real_formula = self.display_formula.innerHTML
-                        real_formula = real_formula.replace('x', '*')
-                        real_formula = real_formula.replace('%', '/100')
-                        const func = new Function('return ' + real_formula)
-                        self.display_result.innerHTML = func()
+                        real_formula = real_formula.replaceAll('x', '*')
+                        //const func = new Function('return ' + real_formula)
+                        //self.display_result.innerHTML = func()
+                        try {
+                            self.display_result.innerHTML = math.evaluate(real_formula)
+                            self.display_error.innerHTML = ''
+                        } catch(err) {
+                            self.display_error.innerHTML = 'Error'
+                        }
                     default:
                         break;
                 }
